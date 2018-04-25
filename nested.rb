8cd9b6ce51c844ebf913155dc0f5fbc549b02d04
Caio Sanches <ccampato@gmail.com>
2018-04-25 23:08:28 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+	programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -121,7 +121,4 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-	programmer_hash[:grace_hopper]
-
 end
